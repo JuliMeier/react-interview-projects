043c95f848e91dc070d5016a36016773bab09653
Julian Meier <julianrg320@gmail.com>
2024-03-06 08:44:00 -0300

    primer proyecto accordion

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: 'What are accordion components?',
+        answer: 'Accordion components are user interface elements used for organizing and presenting content in a collapsible manner'
+    },
+    {
+        id: '2',
+        question: 'What are they used for?',
+        answer: 'They are commonly employed  in various contexts, including FAQS, products description, navigation menues, settings panels and data tables'
+    },
+    {
+        id: '3',
+        question: 'Accordion as a musical instrument?',
+        answer: 'The accordion  is a musical instrument with a keyboard and bellows. It produces sound by air passing over reeds  when the player expands or compress the bellows, used in various music genres'
+    },
+    {
+        id: '4',
+        question: 'Can i create an accordion  component with different?',
+        answer: 'Yes of course, it is very possible to create an accordion component with another framework'
+    },
+];
+
+export default data;
