d68ca936dc563969b6917407ee4e2677545c9b0c
Julian Meier <julianrg320@gmail.com>
2024-03-07 09:13:09 -0300

    proyecto random color

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 
 import Accordion from './components/accordion'
+import RandomColor from './components/random-color'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Accordion />
+      {/* Accordion component */}
+      {/* <Accordion /> */}
+      {/* Random Color Component  */}
+      <RandomColor />
     </>
   )
 }
---
+++ src/components/random-color/index.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+export default function RandomColor() {
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#000000");
+
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function handleClickRandomHexColor() {
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+    setColor(hexColor);
+  }
+  function handleClickRandomRgbColor() {
+    const r = randomColorUtility(256);
+    const g = randomColorUtility(256);
+    const b = randomColorUtility(256);
+
+    setColor(`rgb(${r},${g},${b})`);
+  }
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background: color,
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <button onClick={() => setTypeOfColor("hex")}>Create HEX Color</button>
+        <button onClick={() => setTypeOfColor("rgb")}>Create RGB Color</button>
+        <button
+          onClick={
+            typeOfColor === "hex"
+              ? handleClickRandomHexColor
+              : handleClickRandomRgbColor
+          }
+        >
+          Generate Random Color
+        </button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: '#fff',
+          fontSize: '60px',
+          marginTop: '40px',
+        }}
+      >
+        <h3>{typeOfColor}</h3>
+        <h1>{color}</h1>
+      </div>
+    </div>
+  );
+}
