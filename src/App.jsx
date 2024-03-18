a8c8af456aee37755fe6f7ddb46a7a90ff5a35df
Julian Meier <julianrg320@gmail.com>
2024-03-18 20:35:46 -0300

    finalizado el star rating comenzando el image slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import Accordion from './components/accordion'
-import RandomColor from './components/random-color'
-import StarRating from './components/star-rating'
+
+import Accordion from "./components/accordion";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       {/* Accordion component */}
       {/* <Accordion /> */}
-      {/* Random Color Component  */}
+      {/*Random Color Component */}
       {/* <RandomColor /> */}
-       {/* Star Rating Component  */}
-       <StarRating numOfStars={10}/>
+      {/* Star Rating Component */}
+      {/* <StarRating numOfStars={10} /> */}
+      {/* Image slider Component */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"10"} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
