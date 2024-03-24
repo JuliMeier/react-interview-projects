2f0733f51f5aacc354df7155aaf068e98cd1cfb6
Julian Meier <julianrg320@gmail.com>
2024-03-24 12:19:32 -0300

    qr code terminado

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
+import QRCodeGenerator from "./components/qr-code-generator";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,7 +25,11 @@ function App() {
       {/* Image slider Component */}
       {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"10"} /> */}
       {/* Load more products Component */}
-      <LoadMoreData />
+      {/* <LoadMoreData /> */}
+      {/* Tree view Component */}
+      {/* <TreeView menus={menus} /> */}
+      {/* QR code generatpr */}
+      <QRCodeGenerator />
     </>
   );
 }
